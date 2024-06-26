af06481b544e8c6b54412ba4aaf30f8cffb65d34
Juan Holy <juanholydesouza@gmail.com>
2024-06-26 03:46:20 -0400

    feat: Creating DbContext and DbSets

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+using Blog.Data.Mappings;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data
+{
+    public class BlogDataContext : DbContext
+    {
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseMySql("server=localhost,3306;database=blog;User ID=root;Password=;", new MySqlServerVersion(new Version(8, 0, 2)));
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            modelBuilder.ApplyConfiguration(new PostMap());
+            modelBuilder.ApplyConfiguration(new UserMap());
+        }
+    }
+}
